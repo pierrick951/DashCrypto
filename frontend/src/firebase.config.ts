34382931bf6306387967ac680818766373b4c656
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-08-05 15:56:26 -0400

    update api key

--- frontend/src/firebase.config.ts
+++ frontend/src/firebase.config.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider,Auth } from 'firebase/auth';
-import { firebaseConfigType } from './types/TypeData';
-
-const firebaseConfig:firebaseConfigType = {
-  apiKey: "AIzaSyChXh6QdbnZKBoKSZiboVKLr2PPkcRckbM",
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, Auth } from "firebase/auth";
+import { firebaseConfigType } from "./types/TypeData";
+const APIKEY = import.meta.env.VITE_AUTH_API_KEY;
+const firebaseConfig: firebaseConfigType = {
+  apiKey: APIKEY,
   authDomain: "cryptodashboard-3c36d.firebaseapp.com",
   projectId: "cryptodashboard-3c36d",
-  storageBucket:  "cryptodashboard-3c36d.appspot.com",
+  storageBucket: "cryptodashboard-3c36d.appspot.com",
   messagingSenderId: "879698478554",
   appId: "1:879698478554:web:cdd9ce512e0ce75c69ace3",
-  measurementId: "G-KN9P1VS5X0"
+  measurementId: "G-KN9P1VS5X0",
 };
 
 const app = initializeApp(firebaseConfig);
