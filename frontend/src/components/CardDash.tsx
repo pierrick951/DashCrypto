95994679b9d0d773abbfc06f416f660a03311ff5
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-08-26 10:35:14 -0400

    update logique de recuperation du  current block

--- frontend/src/components/CardDash.tsx
+++ frontend/src/components/CardDash.tsx
@@ -1,14 +1,14 @@
 type Props = {
   title1: string;
   title2: JSX.Element;
-  data?: string | null;
+  data?: any;
 };
 
 const contentCard: string = "loading";
 
 function bodyCard({ title1, title2, data }: Props) {
   return (
-    <div className="bg-gradient-to-tr from-white to-gray-100 rounded p-4 w-full mb-5 shadow-2xl shadow-zinc-800">
+    <div className="bg-gradient-to-tr from-white to-gray-100 rounded p-4 w-full max-w-xs mb-5 shadow-2xl shadow-zinc-800">
       <h2 className="flex flex-row font-semibold  text-zinc-800 items-center  justify-between gap-2 py-2">
         <span >{title1}</span>
         <span>{title2}</span>
