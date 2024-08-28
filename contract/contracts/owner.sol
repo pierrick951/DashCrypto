49e275037032293df9e89b3ec08719affbda9ad4
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-08-28 15:25:39 -0400

    ecriture des smart contract pour l envoie de transaction

--- contract/contracts/owner.sol
+++ contract/contracts/owner.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
+
+contract Owner {
+    address owner;
+
+    constructor() {
+        msg.sender == owner;
+
+    }
+
+    modifier iisOwner() { 
+      require(msg.sender == owner);
+      _;
+    }
+}
--- contract/contracts/send.sol
+++ contract/contracts/send.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  ^0.8.26;
+pragma solidity ^0.8.26;
+
+import "./owner.sol";
+
+contract Sender  is Owner{
 
 
-contract  Sender {
 
+    event newTransaction(address indexed author, uint value);
 
-      function setMoney(address _address, uint _amout) public payable{
-
-      }    
+    function setMoney(address payable _address, uint _amount) public payable {
+        require(address(this).balance >= _amount,"insufisant balance");
+        _address.transfer(_amount);
+        emit newTransaction (msg.sender, _amount);
+    }
 }
