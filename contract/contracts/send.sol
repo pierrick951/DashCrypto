34fc1edb518d77bfba0ffbc2585352c8eef374f7
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-08-30 18:48:44 -0400

    up

--- contract/contracts/send.sol
+++ contract/contracts/send.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "./owner.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Sender  is Owner{
-
-
-
+contract Sender is Ownable {
     event newTransaction(address indexed author, uint value);
+    constructor() Ownable(msg.sender) {}
 
     function setMoney(address payable _address, uint _amount) public payable {
-        require(address(this).balance >= _amount,"insufisant balance");
+        require(msg.value >= _amount, "insufisant balance");
+        require(_address != address(0), "Invalid address");
         _address.transfer(_amount);
-        emit newTransaction (msg.sender, _amount);
+        emit newTransaction(msg.sender, _amount);
     }
 }
