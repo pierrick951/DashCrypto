541c7ef19ff3ba098ebe78b85fbee4a04a9df1b9
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-10-01 15:27:26 -0400

    up

--- backend/contracts/Token.sol
+++ backend/contracts/Token.sol
@@ -6,19 +6,23 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20, Ownable, ReentrancyGuard {
-    uint256 initialSuply = 1_000_000;
+    uint256 initialSuply;
     uint tokenPrice = 100 wei;
+
     event TokenPurchased(address indexed buyer, uint256 amount);
 
-    constructor() ERC20("Yuzu", "YZU") Ownable(msg.sender) {
-        _mint(address(this), initialSuply);
+    constructor(
+        uint256 _initialSuply
+    ) ERC20("Yuzu", "YZU") Ownable(msg.sender) {
+        initialSuply = _initialSuply;
+        _mint(address(this), _initialSuply);
     }
 
     function burnToken(uint256 _amount) public onlyOwner {
         _burn(address(this), _amount);
     }
 
-    function buyToken(uint256 _amountToken) public payable nonReentrant {
+        function buyToken(uint256 _amountToken) public payable nonReentrant {
         require(
             msg.value >= _amountToken * tokenPrice,
             "Not enough funds provided"
@@ -32,4 +36,7 @@ contract Token is ERC20, Ownable, ReentrancyGuard {
         emit TokenPurchased(msg.sender, _amountToken);
     }
 
+    function getSuplly() public view returns (uint256)  {
+        return balanceOf(address(this));
+    }
 }
---
+++ backend/test/Token.ts
@@ -0,0 +1,61 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { Token__factory, Token } from "../typechain-types";
+
+describe("Token", function () {
+  let user1: SignerWithAddress;
+  let owner: SignerWithAddress;
+  let token: Token;
+
+  beforeEach("token Factory", async function () {
+    const tokenFactory = (await ethers.getContractFactory(
+      "Token"
+    )) as Token__factory;
+    [owner, user1] = await ethers.getSigners();
+    token = await tokenFactory.deploy(1000);
+  });
+
+  describe("admin", function () {
+    it("Admin should burn token", async function() {
+        const suplly = await token.getSuplly();
+        const burnAmount = 2n; 
+    
+        await token.connect(owner).burnToken(burnAmount); 
+        const finalSuplly = await token.getSuplly(); 
+    
+        expect(finalSuplly).to.equal(suplly - burnAmount); 
+    });
+
+    it("user should burn token", async function () {
+      const burnAmount = 2n;
+      await expect(
+        token.connect(user1).burnToken(burnAmount)
+      ).to.be.revertedWithCustomError(token, "OwnableUnauthorizedAccount");
+    });
+  });
+
+  describe("buy token ", function () {
+    it("Should buy token", async function () {
+      const tokenPrice = 100;
+      const amountToBuy = 1;
+      const cost = amountToBuy * tokenPrice;
+
+      await expect(token.connect(user1).buyToken(amountToBuy, { value: cost }))
+        .to.emit(token, "TokenPurchased")
+        .withArgs(user1.address, amountToBuy);
+      const userBalance = await token.balanceOf(user1.address);
+      expect(userBalance).to.equal(amountToBuy);
+    });
+
+    it("Amount should  is small than contract balance", async function () {
+      const tokenPrice = 100;
+      const amountToBuy = 1;
+      const insufficientValue = tokenPrice - 1;
+
+      await expect(
+        token.connect(user1).buyToken(amountToBuy, { value: insufficientValue })
+      ).to.be.revertedWith("Not enough funds provided");
+    });
+  });
+});
