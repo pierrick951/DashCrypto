62a6885513857e6fb1e8ed78e30c1fb45ab99750
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-10-04 13:54:10 -0400

    up

--- backend/scripts/DeployGovernance.ts
+++ backend/scripts/DeployGovernance.ts
@@ -1,12 +1,12 @@
 import { ethers } from "hardhat";
-
+//   0x5Da5E19db8b47f0427e9497260b637eb77aF563B
 async function main() {
 
     
   const tokenAddress = "0x1ea675656b01d4E0aD07AdA79BC18866E147808D";
 
   const GovernanceFcatory = await ethers.getContractFactory("Governance");
-  const governance = await GovernanceFcatory.deploy(tokenAddress);
+  const governance = await GovernanceFcatory.deploy(tokenAddress, { gasLimit:  400000 });
 
   console.log("stacking deployed at address", await governance.getAddress());
 
--- backend/scripts/DeployStacking.ts
+++ backend/scripts/DeployStacking.ts
@@ -7,7 +7,7 @@ async function main() {
     const tokenAddress = "0x1ea675656b01d4E0aD07AdA79BC18866E147808D"
 
     const  StackingFactory = await ethers.getContractFactory("Stacking")
-    const stacking = await StackingFactory.deploy(tokenAddress);
+    const stacking = await StackingFactory.deploy(tokenAddress, { gasLimit:  300000 });
 
     console.log('stacking deployed at address', await stacking.getAddress());
 
